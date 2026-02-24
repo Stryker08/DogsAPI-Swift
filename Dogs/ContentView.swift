dd6d943db1092beef2f3731627b0b8dd4f225514
Stryker <stryker.shaw@fhnw.edu>
2026-02-24 09:59:00 -0700

    feat: Added our server data to our image

--- Dogs/ContentView.swift
+++ Dogs/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var doggoImage: URL?
     var body: some View {
         VStack {
-          AsyncImage(url: URL(string: "https://images.dog.ceo/breeds/komondor/n02105505_2489.jpg")){img in
+          AsyncImage(url: doggoImage){img in
                 if let error = img.error{
                     Text("We Have An Error")
               Text("\(error.localizedDescription)")
@@ -25,12 +26,49 @@ struct ContentView: View {
             Text("Frank")
             .bold()
             .font(Font.largeTitle)
+          Button {
+            Task {
+              let ourData = await getServerData()
+              if let ourData = ourData {
+                doggoImage = URL(string:
+                  ourData.message)
+              }
+            }
+          } label: {
+            Text("Fetch new Doggo")
+          }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
     }
+  func getServerData() async -> ServerResponse? {
+    do {
+      guard let serverURL = URL(string: "https://dog.ceo/api/breeds/image/random") else {
+        return nil
+      }
+      let (data, response) = try await
+      URLSession.shared.data(from: serverURL)
+      guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+        print("We got a bad status code!")
+        return nil
+      }
+      let decoded = try JSONDecoder().decode(ServerResponse
+        .self, from: data)
+      return decoded
+      
+    } catch {
+      print(error)
+    }
+    return nil
+  }
 }
+
 
 #Preview {
     ContentView()
+}
+
+struct ServerResponse: Codable {
+  let message: String
+  let status: String
 }

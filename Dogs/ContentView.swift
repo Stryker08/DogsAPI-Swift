f4f20800d31bd7dab69eb429c5f579c72bb3f961
Stryker <stryker.shaw@fhnw.edu>
2026-02-24 09:40:12 -0700

    feat: Added image from Dog.CEO

--- Dogs/ContentView.swift
+++ Dogs/ContentView.swift
@@ -10,11 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          AsyncImage(url: URL(string: "https://images.dog.ceo/breeds/komondor/n02105505_2489.jpg")){img in
+                if let error = img.error{
+                    Text("We Have An Error")
+              Text("\(error.localizedDescription)")
+            }
+            if let image = img.image {
+              image
+                .resizable()
+                .frame(width: 200, height: 200)
+            }
+            }
+                
+            Text("Frank")
+            .bold()
+            .font(Font.largeTitle)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
     }
 }
